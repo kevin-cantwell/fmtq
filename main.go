a0d1c5fa77874b1cf7edfbda0ed0018d03bc73ff
Kevin Cantwell <kevin.cantwell@gmail.com>
2017-02-08 13:58:22 -0500

    support -s single quote flag

--- main.go
+++ main.go
@@ -9,15 +9,26 @@ import (
 )
 
 func main() {
-	os.Stdout.Write([]byte(`"`))
-	defer os.Stdout.Write([]byte("\"\n"))
+	useSingleQuotes := len(os.Args) > 1 && os.Args[1] == "-s"
+	if useSingleQuotes {
+		os.Stdout.Write([]byte(`'`))
+		defer os.Stdout.Write([]byte("'\n"))
+	} else {
+		os.Stdout.Write([]byte(`"`))
+		defer os.Stdout.Write([]byte("\"\n"))
+	}
 	reader := bufio.NewReader(os.Stdin)
 	for {
 		r, _, err := reader.ReadRune()
 		if err == io.EOF {
 			return
 		}
-		escaped := []byte(strconv.QuoteRune(r))
+		var escaped []byte
+		if useSingleQuotes {
+			escaped = []byte(strconv.QuoteRune(r))
+		} else {
+			escaped = []byte(strconv.Quote(string(r)))
+		}
 		if _, err := os.Stdout.Write(escaped[1 : len(escaped)-1]); err != nil {
 			log.Fatal(err)
 		}
