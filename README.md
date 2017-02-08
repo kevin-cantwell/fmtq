e6a8a133074c320155f476f3f49d8211c3a7837c
Kevin Cantwell <kevin.cantwell@gmail.com>
2017-02-08 13:49:15 -0500

    add readme

---
+++ README.md
@@ -0,0 +1,14 @@
+The `fmtq` command tool simply parses stdin and returns a double-quoted string safely escaped with Go syntax. It's essentially a command-line equivalent to the print verb `%q` from Go's [fmt](https://golang.org/pkg/fmt/) package.
+
+## Installation
+
+`go get -u github.com/kevin-cantwell/fmtq`
+
+## Usage
+
+```
+$ echo "foo
+> bar
+> baz" | fmtq
+"foo\nbar\nbaz\n"
+```
---
+++ main.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"bufio"
+	"io"
+	"log"
+	"os"
+	"strconv"
+)
+
+func main() {
+	os.Stdout.Write([]byte(`"`))
+	defer os.Stdout.Write([]byte("\"\n"))
+	reader := bufio.NewReader(os.Stdin)
+	for {
+		r, _, err := reader.ReadRune()
+		if err == io.EOF {
+			return
+		}
+		escaped := []byte(strconv.QuoteRune(r))
+		if _, err := os.Stdout.Write(escaped[1 : len(escaped)-1]); err != nil {
+			log.Fatal(err)
+		}
+	}
+}
